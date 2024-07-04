ad83be4e0fdcbfd2662ee4ee15b8de309711f3b3
CydoEntis <cydoentis@gmail.com>
2024-07-04 15:55:16 -0400

    Added login/signup page installed MUI

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { Button, Link, Stack, TextField } from "@mui/material";
 
-type Props = {}
+import NextLink from "next/link";
+import React from "react";
+
+type Props = {};
 
 function Login({}: Props) {
 	return (
-		<div>Login</div>
-	)
+		<Stack
+			spacing={2}
+			className="w-full max-w-xs"
+		>
+			<TextField
+				label="Email"
+				variant="outlined"
+				type="email"
+			/>
+			<TextField
+				label="Password"
+				variant="outlined"
+				type="password"
+			/>
+			<Button variant="contained">Login</Button>
+			<Link component={NextLink} href="/auth/signup" className="self-center">Sign Up</Link>
+		</Stack>
+	);
 }
 
-export default Login
+export default Login;
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { Button, Link, Stack, TextField } from "@mui/material";
 
-type Props = {}
+import NextLink from "next/link";
+import React from "react";
+
+type Props = {};
 
 function Signup({}: Props) {
 	return (
-		<div>Signup</div>
-	)
+		<Stack
+			spacing={2}
+			className="w-full max-w-xs"
+		>
+			<TextField
+				label="Email"
+				variant="outlined"
+				type="email"
+			/>
+			<TextField
+				label="Password"
+				variant="outlined"
+				type="password"
+			/>
+			<Button variant="contained">Sign Up</Button>
+			<Link
+				component={NextLink}
+				href="/auth/login"
+				className="self-center"
+			>
+				Login
+			</Link>
+		</Stack>
+	);
 }
 
-export default Signup
+export default Signup;
